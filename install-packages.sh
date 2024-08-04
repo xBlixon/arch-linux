e9e9698f9c1210944e2cade814ae5957b76f588d
Xawier Slupik <48760619+xBlixon@users.noreply.github.com>
2024-08-04 03:13:07 +0200

    added intel graphics packages

--- install-packages.sh
+++ install-packages.sh
@@ -3,4 +3,5 @@ pacstrap -K /mnt base linux linux-firmware intel-ucode \
                 iwd networkmanager rp-pppoe modemmanager \
                 wpa_supplicant dhclient \
                 man-db man-pages texinfo \
-                nano fzf bashtop sudo git bash-completion
+                nano fzf bashtop sudo git bash-completion \
+                mesa mesa-utils vulkan-intel vulkan-icd-loader
